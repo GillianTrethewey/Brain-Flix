8c0371926c5fca21cf1ba375c77f40109b5d78d6
Gillian Trethewey <gilliantrethewey100@gmail.com>
2024-01-19 10:07:46 -0800

    adds video responsive styling

--- src/components/HeroVideo/HeroVideo.js
+++ src/components/HeroVideo/HeroVideo.js
@@ -4,10 +4,14 @@ const HeroVideo = () => {
   return (
     <section className="hero-video__container">
       <video
+        width="100%"
         className="hero-video"
         controls
         alt="featured video section"
-        poster="https://project-2-api.herokuapp.com/images/image0.jpg"></video>
+        poster="https://project-2-api.herokuapp.com/images/image0.jpg">
+          <source src="" type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
     </section>
   );
 };
